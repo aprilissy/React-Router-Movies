8c483b6741e013505bc6c8932c2fa968fdf8891a
April Darger <april.m.darger@gmail.com>
2020-10-12 14:14:06 -0600

    Set the movie data and added it as props.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { Route, Switch, Link } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -13,6 +15,9 @@ export default function App() {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data)
+          //console.log('data', response.data);
+
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -22,6 +27,7 @@ export default function App() {
     }
     getMovies();
   }, []);
+  console.log('movieList', movieList);
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
@@ -32,20 +38,21 @@ export default function App() {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-
       <Switch>
 
-        {/* one route for `/` that loads the `MovieList` component. This component will need the movies injected into it via props. */}
-        <Route>
-          <MovieList movies={} />
+        {/*  one route that will take an `id` parameter after`/movies/` (ex: `/movies/2`, `/movies/3` where the id is dynamic). This route should load the `Movie` component. */}
+        <Route path={`/movies/${movieList.id}`}>
+          <Movie />
         </Route>
 
-        {/*  one route that will take an `id` parameter after`/movies/` (ex: `/movies/2`, `/movies/3` where the id is dynamic). This route should load the `Movie` component. */}
-        <Route>
-          <Movie id={} />
+        {/* one route for `/` that loads the `MovieList` component. This component will need the movies injected into it via props. */}
+        <Route path={'/'}>
+          <MovieList movies={movieList} />
         </Route>
 
       </Switch>
+
+
     </div>
   );
 }
